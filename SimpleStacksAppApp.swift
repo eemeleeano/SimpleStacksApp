b55e66bd5f375cb3233e86eb4963aeae1019eb74
Jason Ingram <jason.ingram@firmecoding.org>
2025-06-09 11:15:37 -0700

    Lesson Upload

---
+++ SimpleStacksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleStacksAppApp.swift
+//  SimpleStacksApp
+//
+//  Created by Jason Ingram on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleStacksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
